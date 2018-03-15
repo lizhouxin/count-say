143dc57186831e578f3b8904e95fed105b5db897
薪 李 <lizhouxin@live.com>
2018-03-15 21:02:35 +0800

    用于堆排序，大顶堆原则，初始堆，由下到上，由左到右，根据完全二叉树原理，节点是个数除2，后面将根节点与最后一个叶子调换，重新构建堆。

---
+++ leTest/heap.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+void HeapAjust(vector<int> &vec,int num,int length)
+{
+	int temp,j;
+	temp = vec[num];
+	for (j = 2 * num; j <= length; j *= 2)
+	{
+		if (j < length && vec[j] < vec[j + 1])
+			j = j + 1;
+		if (vec[num] >= vec[j])
+			break;
+		vec[num] = vec[j];
+		num = j;
+		vec[num] = temp;
+	}
+	
+}
+int main()
+{
+	vector<int> inVec;
+	int m;
+	inVec.push_back(0);
+	do
+	{
+		cin >> m;
+		inVec.push_back(m);
+	} while (cin.get() != '\n');
+
+	//´î½¨´ó¶¥ÍÆ
+	int length = inVec.size() - 1;
+	for (int i = length / 2; i > 0; i--)
+	{
+		HeapAjust(inVec, i, length);
+	}
+
+	for (int i = length; i > 1; i--)
+	{
+		swap(inVec[1], inVec[i]);
+		HeapAjust(inVec, 1, i - 1);
+	}
+	for(int i = 1;i <= length;i++)
+	cout << inVec[i] << " ";
+	system("pause");
+}
